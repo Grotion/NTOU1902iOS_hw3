dc9cd53ee26b76dc64af70b7d01fa558fe271e16
GroTion <grotion0720@gmail.com>
2021-03-31 11:07:08 +0800

    Initial Commit

---
+++ ntou1902iOS_hw3/ntou1902iOS_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  ntou1902iOS_hw3App.swift
+//  ntou1902iOS_hw3
+//
+//  Created by Shaun Ku on 2021/3/31.
+//
+
+import SwiftUI
+
+@main
+struct ntou1902iOS_hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
